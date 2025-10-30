fadc50bd78334e30562995cc5c071579416e84b0
tccmw <jungmwoo6248@gmail.com>
2025-10-30 12:00:30 +0900

    refactor: button -> div 태그 교체

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -48,7 +48,7 @@ export default function Button({
   }
 
   return (
-    <button
+    <div
       onClick={onClick}
       className={classNames(
         config.font,
@@ -64,6 +64,6 @@ export default function Button({
       {(type === "team" || type === "teamGray400") && (
         <Image src={ChevronRight} alt="chevron" />
       )}
-    </button>
+    </div>
   );
 }
